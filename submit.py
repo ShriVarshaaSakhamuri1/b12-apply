916156a39784595809e4de6e11395bb1b0326f2c
Shricodes <79305212+ShriVarshaaSakhamuri1@users.noreply.github.com>
2026-01-28 12:20:36 -0600

    Update submit.py with environment variables and links

--- submit.py
+++ submit.py
@@ -1,17 +1,25 @@
 import json
 import hmac
 import hashlib
+import os
 from datetime import datetime, timezone
 
 SIGNING_SECRET = b"hello-there-from-b12"
 
 def main():
+    repository = os.environ["GITHUB_REPOSITORY"]
+    run_id = os.environ["GITHUB_RUN_ID"]
+
+    action_run_link = (
+        f"https://github.com/{repository}/actions/runs/{run_id}"
+    )
+
     payload = {
-        "action_run_link": "ACTION_RUN_LINK_PLACEHOLDER",
-        "email": "your-email@example.com",
+        "action_run_link": action_run_link,
+        "email": "shrivarshaasakhamuri@gmail.com",
         "name": "Shrivarshaa Sakhamuri",
-        "repository_link": "https://github.com/<your-github-username>/b12-apply",
-        "resume_link": "https://www.linkedin.com/in/shrivarshaa-sakhamuri-3a22091a7",
+        "repository_link":  "https://github.com/ShriVarshaaSakhamuri1/b12-apply",
+        "resume_link": "https://www.linkedin.com/in/shrivarshaa-sakhamuri",
         "timestamp": datetime.now(timezone.utc)
             .isoformat(timespec="milliseconds")
             .replace("+00:00", "Z"),
